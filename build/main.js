24e0804c392d9b8a8bb17cf808109ac8ead67d79
Damien Guard <damieng@gmail.com>
2017-02-28 11:05:04 -0800

    Initial version

---
+++ build/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _omnisharpLanguageServer = require('./omnisharp-language-server');
+
+var _omnisharpLanguageServer2 = _interopRequireDefault(_omnisharpLanguageServer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = new _omnisharpLanguageServer2.default();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2xpYi9tYWluLmpzIl0sIm5hbWVzIjpbIm1vZHVsZSIsImV4cG9ydHMiXSwibWFwcGluZ3MiOiI7O0FBQUE7Ozs7OztBQUVBQSxPQUFPQyxPQUFQLEdBQWlCLHVDQUFqQiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IE9tbmlzaGFycExhbmd1YWdlU2VydmVyIGZyb20gJy4vb21uaXNoYXJwLWxhbmd1YWdlLXNlcnZlcic7XHJcblxyXG5tb2R1bGUuZXhwb3J0cyA9IG5ldyBPbW5pc2hhcnBMYW5ndWFnZVNlcnZlcigpO1xyXG4iXX0=
